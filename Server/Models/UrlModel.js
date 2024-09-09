44b2bd572ffa7645a029faa323d04d39b9886582
Anshika2608 <anshika2210142@akgec.ac.in>
2024-09-10 00:32:40 +0530

    Updated dependencies and removed node_modules

--- Server/Models/UrlModel.js
+++ Server/Models/UrlModel.js
@@ -1,18 +1,21 @@
-const mongoose=require("mongoose");
-const {v4:uuidv4}=require("uuid")
-const  generateShortuuid=()=>{
-    return uuidv4().replace(/-/g,"").slice(0,8)
-}
-const urlSchema= new mongoose.Schema({
-    UrlId:{
-      type:String,
-      required:true,
-      unique:true,
-      default:generateShortuuid
-    },
-    redirectUrl:{
-        type:String,
-        required:true
-    },
-})
-module.exports=new mongoose.model("urls",urlSchema)
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+
+const generateShortuuid = () => {
+  return uuidv4().replace(/-/g, '').slice(0, 8);
+};
+
+const urlSchema = new mongoose.Schema({
+  UrlId: {
+    type: String,
+    required: true,
+    unique: true,
+    default: generateShortuuid
+  },
+  redirectUrl: {
+    type: String,
+    required: true
+  },
+});
+
+module.exports = mongoose.model('Url', urlSchema);
