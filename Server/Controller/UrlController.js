78e2eb4b93e9570bb8c9b500e7bfd4c64283cde4
Anshika2608 <anshika2210142@akgec.ac.in>
2024-09-10 00:25:37 +0530

    urlController_updated

--- Server/Controller/UrlController.js
+++ Server/Controller/UrlController.js
@@ -1,4 +1,3 @@
-const shortid=require("shortid")
 const urlModel=require("../Models/UrlModel")
 const submitUrl = async (req, res) => {
     try {
@@ -7,15 +6,14 @@ const submitUrl = async (req, res) => {
             return res.status(400).json({ message: 'URL is required!' });
         }
 
-        const UrlId = shortid.generate(); 
 
         const newUrl = new urlModel({
-            UrlId: UrlId,
+            
             redirectUrl: url
         });
 
         await newUrl.save();
-        return res.status(201).json({ message: 'Short URL successfully created', UrlId: UrlId });
+        return res.status(201).json({ message: 'Short URL successfully created' });
     } catch (err) {
         return res.status(500).json({ message: 'Error while generating the short URL', error: err.message });
     }
--- Server/Models/UrlModel.js
+++ Server/Models/UrlModel.js
@@ -1,9 +1,14 @@
 const mongoose=require("mongoose");
+const {v4:uuidv4}=require("uuid")
+const  generateShortuuid=()=>{
+    return uuidv4().replace(/-/g,"").slice(0,8)
+}
 const urlSchema= new mongoose.Schema({
     UrlId:{
       type:String,
       required:true,
-      unique:true
+      unique:true,
+      default:generateShortuuid
     },
     redirectUrl:{
         type:String,
