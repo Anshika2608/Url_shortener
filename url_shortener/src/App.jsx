db901bd0e8f318b2f5504811680704f82e1046e0
Anshika2608 <anshika2210142@akgec.ac.in>
2024-09-13 22:40:49 +0530

    input_box_completed

--- url_shortener/src/App.jsx
+++ url_shortener/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import InputComponent from './Components/InputCompo';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='underline text-2xl'> hello world</h1>
-        
+      <div className='flex w-screen h-screen justify-center items-center '>
+        <InputComponent />
+      </div>
+
+
     </>
   )
 }
