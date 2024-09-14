6e0c216ad164970c660d1a9585920c440214c270
Anshika2608 <anshika2210142@akgec.ac.in>
2024-09-14 22:45:02 +0530

    particle_effect_added

--- url_shortener/src/App.jsx
+++ url_shortener/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import InputComponent from './Components/InputCompo';
+import ParticlesComponent from './Components/ParticlesBack';
+// import ParticlesBackground from './Components/ParticlesBack';
 function App() {
   return (
     <>
-      <div className='flex w-screen h-screen justify-center items-center '>
-        <InputComponent />
+    <div className='App'>
+      <ParticlesComponent id="particles"/>
+      <div className='flex justify-center items-center w-full h-screen'>
+        <InputComponent/>
       </div>
+    </div>
+     
     </>
   )
 }
--- url_shortener/src/Components/InputCompo.jsx
+++ url_shortener/src/Components/InputCompo.jsx
@@ -6,6 +6,7 @@ function InputComponent() {
   const handleSubmit = async () => {
     try {
       const response = await axios.post("https://url-shortener-yj9n.onrender.com/submitUrl", { url });
+      console.log(response.data)
       setUrlId(response.data.urlId)
       setUrl("")
     } catch (error) {
@@ -14,13 +15,13 @@ function InputComponent() {
   };
   return (
     <>
-      <div className='flex flex-col  w-[34rem] rounded-lg border-2 border-black  bg-neutral-100  justify-center items-center h-72 gap-4'>
+      <div className='flex flex-col  w-[34rem] rounded-lg border-2 border-black  bg-neutral-300  justify-center items-center h-72 gap-4'>
         <div className='flex gap-4 flex-col'>
           <h2 className='text-2xl font-semibold self-start '>Enter URL</h2>
           <input type="text" value={url} name="url" placeholder="Type here..." onChange={e => setUrl(e.target.value)} className='border-2 border-black rounded-lg w-96 h-12 p-2' />
         </div>
         {urlId && (
-          <div className='text-md text-green-500'>URL ID: {urlId}</div>
+          <div className='text-md  text-green-800'>URL ID: {urlId}</div>
         )}      
         <button className='h-10 w-40 rounded-lg  bg-slate-900 text-white text-xl font-semibold' onClick={handleSubmit}>
           Submit
