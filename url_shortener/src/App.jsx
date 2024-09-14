134c242671b4bc56e4c256afb84a7d456dde5259
Anshika2608 <anshika2210142@akgec.ac.in>
2024-09-14 23:26:38 +0530

    logo_added

--- url_shortener/src/App.jsx
+++ url_shortener/src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
 import InputComponent from './Components/InputCompo';
 import ParticlesComponent from './Components/ParticlesBack';
-// import ParticlesBackground from './Components/ParticlesBack';
+import logo from "./assets/logo.png"
 function App() {
   return (
     <>
     <div className='App'>
       <ParticlesComponent id="particles"/>
-      <div className='flex justify-center items-center w-full h-screen'>
+      {/* <img src={logo} className='h-28 w-56 z-10'/> */}
+      <div >
+      <img src={logo} className='h-28 w-56 z-10 '/>
+
+        <div className='flex justify-center items-center w-full h-screen -mt-44'>
         <InputComponent/>
+
+        </div>
       </div>
     </div>
-     
     </>
   )
 }
--- url_shortener/src/Components/InputCompo.jsx
+++ url_shortener/src/Components/InputCompo.jsx
@@ -6,7 +6,6 @@ function InputComponent() {
   const handleSubmit = async () => {
     try {
       const response = await axios.post("https://url-shortener-yj9n.onrender.com/submitUrl", { url });
-      console.log(response.data)
       setUrlId(response.data.urlId)
       setUrl("")
     } catch (error) {
@@ -15,10 +14,11 @@ function InputComponent() {
   };
   return (
     <>
-      <div className='flex flex-col  w-[34rem] rounded-lg border-2 border-black  bg-neutral-300  justify-center items-center h-72 gap-4'>
+      <div className='flex flex-col w-72 sm:w-[34rem] rounded-lg border-2   bg-neutral-300  justify-center items-center h-60 sm:h-72 gap-4'>
+       
         <div className='flex gap-4 flex-col'>
           <h2 className='text-2xl font-semibold self-start '>Enter URL</h2>
-          <input type="text" value={url} name="url" placeholder="Type here..." onChange={e => setUrl(e.target.value)} className='border-2 border-black rounded-lg w-96 h-12 p-2' />
+          <input type="text" value={url} name="url" placeholder="Type here..." onChange={e => setUrl(e.target.value)} className='border-2 border-black rounded-lg  w-60 sm:w-96 h-12 p-2' />
         </div>
         {urlId && (
           <div className='text-md  text-green-800'>URL ID: {urlId}</div>
